e3fe9141721d5d501fe37cbe898dcca765a242a6
Arno Peters <info@figment-it.com>
2012-03-29 14:56:24 +0200

    CUT: add offset parameters, center cut on page when omitted

--- cut.js
+++ cut.js
@@ -1,21 +1,32 @@
 function cutUsage() {
     var err = System.err;
 
-    err.print("Usage:  cut.js width height\n");
+    err.print("Usage:  cut.js width height [xoffset yoffset]\n");
     err.print("  Cut PDF to width, height.\n");
     err.print("  width / height in mm, default to A4 when omitted.\n");
+    err.print("  centers cut on page when offsets omitted.\n");
     quit(1);
 }
 
 function checkCutArgs(args) {
     var width;
     var height;
+    var xoffset = 0;
+    var yoffset = 0;
+    var center = true;
+
     if (args.length == 0) {
 	width = mm2pt(210);
 	height = mm2pt(297);
     } else if (args.length == 2) {
 	width = mm2pt(parseInt(args[0]));
 	height = mm2pt(parseInt(args[1]));
+    } else if (args.length == 4) {
+	width = mm2pt(parseInt(args[0]));
+	height = mm2pt(parseInt(args[1]));
+	xoffset = mm2pt(parseInt(args[2]));
+	yoffset = mm2pt(parseInt(args[3]));
+	center = false;
     } else {
 	cutUsage();
     }
@@ -28,7 +39,9 @@ function checkCutArgs(args) {
 	cutUsage();
     }
 
-    return { width: width, height: height }
+    return { width: width, height: height,
+	     xoffset: xoffset, yoffset: yoffset,
+	     center: center }
 }
 
 function cut(args) {
@@ -37,6 +50,7 @@ function cut(args) {
     var params = checkCutArgs(args);
     var wd = params["width"];
     var ht = params["height"];
+    var center = params["center"];
 
     var stdin = new BufferedInputStream(System["in"]);
     var pdfIn = new PdfReader(stdin);
@@ -51,7 +65,7 @@ function cut(args) {
 
     var pageCount = pdfIn.getNumberOfPages();
 
-    err.println("Cut to " + wd + "x" + ht + "mm");
+    err.println("Cut to " + pt2mm(wd) + "x" + pt2mm(ht) + "mm");
     err.println("Pages: " + pageCount);
 
     var cb = writer.getDirectContent();
@@ -67,8 +81,13 @@ function cut(args) {
 
 	var c = 1;
 	var s = 0;
-	var x = - 0.5 * (ws - wd);
-	var y = - 0.5 * (hs - ht);
+	if (center) {
+	    var x = - 0.5 * (ws - wd);
+	    var y = - 0.5 * (hs - ht);
+	} else {
+	    var x = params["xoffset"];
+	    var y = - (hs - ht) + params["yoffset"];
+	}
 	cb.addTemplate(pdfPage, c, s, -s, c, x, y);
 
 	err.print("]");
